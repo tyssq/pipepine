be98e357830f2861f53c671d89462ac9b8974263
lzh <liaozh@tapgo.me>
2019-04-01 14:11:55 +0800

    Signed-off-by: lzh <liaozh@tapgo.me>

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,8 @@
+FROM hub.c.163.com/library/java:8-alpine
+RUN apk add -U tzdata
+RUN cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+ADD target/*.jar app.jar
 
+EXPOSE 8080
+
+ENTRYPOINT ["java", "-jar", "/app.jar"]
---
+++ src/main/java/com/example/pipeline/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.pipeline.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 蜻蜓
+ * @date 2019-04-01 13:55
+ */
+@RestController
+public class DemoController {
+
+    @GetMapping("demo")
+    public Object demo(){
+        return "廖正瀚";
+    }
+}
